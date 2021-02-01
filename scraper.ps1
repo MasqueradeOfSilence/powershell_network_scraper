f6198c5cd20d957d5ab299a20a43d27eb98dca78
Alex <alexcn711@gmail.com>
2021-01-31 19:35:36 -0700

    fixed file issues

--- scraper.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "Hello Get Request!"
-
-$InputWebsite = Read-Host "Please enter the website"
-
-$InputWebsite = "https://" + $InputWebsite
-
-$Result = Invoke-WebRequest -UseBasicParsing -Uri $InputWebsite -Method Get
-
-Write-Output "Result: " $Result
